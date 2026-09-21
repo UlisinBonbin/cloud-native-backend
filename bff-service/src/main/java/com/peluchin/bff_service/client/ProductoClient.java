1644d7fbbaf851f24d3234100653b2cf42d82f8e
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2026-09-21 14:31:00 -0300

    Se avanzo en el productoclient

--- bff-service/src/main/java/com/peluchin/bff_service/client/ProductoClient.java
+++ bff-service/src/main/java/com/peluchin/bff_service/client/ProductoClient.java
@@ -1,4 +1,40 @@
 package com.peluchin.bff_service.client;
 
+import com.peluchin.bff_service.dto.ProductoResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Component
 public class ProductoClient {
+
+    private final RestClient restClient;
+
+    public ProductoClient(
+            RestClient.Builder builder,
+            @Value("${producto-service.url}") String productoServiceUrl) {
+
+        this.restClient = builder
+                .baseUrl(productoServiceUrl)
+                .build();
+    }
+
+    public List<ProductoResponse> obtenerProductos() {
+
+        return restClient.get()
+                .uri("/api/v1/productos")
+                .retrieve()
+                .body(new org.springframework.core.ParameterizedTypeReference<>() {});
+    }
+
+    public ProductoResponse obtenerProducto(Long id) {
+
+        return restClient.get()
+                .uri("/api/v1/productos/{id}", id)
+                .retrieve()
+                .body(ProductoResponse.class);
+    }
+
 }
--- bff-service/src/main/java/com/peluchin/bff_service/controller/ProductoController.java
+++ bff-service/src/main/java/com/peluchin/bff_service/controller/ProductoController.java
@@ -1,4 +1,33 @@
 package com.peluchin.bff_service.controller;
 
+import com.peluchin.bff_service.client.ProductoClient;
+import com.peluchin.bff_service.dto.ProductoResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productos")
 public class ProductoController {
+
+    private final ProductoClient productoClient;
+
+    public ProductoController(ProductoClient productoClient) {
+        this.productoClient = productoClient;
+    }
+
+
+    @GetMapping
+    public List<ProductoResponse> obtenerProductos() {
+        return productoClient.obtenerProductos();
+    }
+
+    @GetMapping("/{id}")
+    public ProductoResponse obtenerProducto(@PathVariable Long id) {
+        return productoClient.obtenerProducto(id);
+    }
+
 }
