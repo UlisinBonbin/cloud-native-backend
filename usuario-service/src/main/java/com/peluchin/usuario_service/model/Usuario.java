1aed7e0fe56f5923c0c03c35ff647e541ba87873
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2026-09-21 13:56:25 -0300

    Se avanzo un poco en el servicio de usuario

--- usuario-service/src/main/java/com/peluchin/usuario_service/model/Usuario.java
+++ usuario-service/src/main/java/com/peluchin/usuario_service/model/Usuario.java
@@ -1,4 +1,28 @@
 package com.peluchin.usuario_service.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String cognitoSub;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
 }
