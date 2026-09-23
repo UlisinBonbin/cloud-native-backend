6887d34717f56f625b3da1ff326ac8778ef7f01e
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2026-09-23 12:20:38 -0300

    Se cambio algo en el security config del bff

--- bff-service/src/main/java/com/peluchin/bff_service/config/SecurityConfig.java
+++ bff-service/src/main/java/com/peluchin/bff_service/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
 
 @Configuration
 public class SecurityConfig {
@@ -19,7 +20,8 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 )
                 .oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt());
+                        oauth2.jwt(Customizer.withDefaults())
+                );
 
         return http.build();
     }
