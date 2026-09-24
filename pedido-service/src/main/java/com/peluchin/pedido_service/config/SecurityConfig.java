21316ae8b528b962f6d303f0adba0f76cd977b09
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2026-09-24 14:15:18 -0300

    Se agregaron algunos cambios

--- pedido-service/src/main/java/com/peluchin/pedido_service/config/SecurityConfig.java
+++ pedido-service/src/main/java/com/peluchin/pedido_service/config/SecurityConfig.java
@@ -10,18 +10,36 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final CognitoJwtAuthenticationConverter jwtAuthenticationConverter;
+
+    public SecurityConfig(
+            CognitoJwtAuthenticationConverter jwtAuthenticationConverter) {
+        this.jwtAuthenticationConverter = jwtAuthenticationConverter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
+
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/pedidos/**").authenticated()
-                        .anyRequest().permitAll()
+
+                        // Todo lo relacionado con pedidos/carrito
+                        // requiere que el usuario haya iniciado sesión.
+                        .requestMatchers("/api/v1/pedidos/**")
+                        .authenticated()
+
+                        .anyRequest().authenticated()
                 )
+
                 .oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt(jwt -> {})
+                        oauth2.jwt(jwt ->
+                                jwt.jwtAuthenticationConverter(
+                                        jwtAuthenticationConverter
+                                )
+                        )
                 );
 
         return http.build();
