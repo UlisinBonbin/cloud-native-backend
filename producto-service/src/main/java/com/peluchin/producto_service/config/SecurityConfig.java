1ee8f20b783570059e0e6a00ea362d0c7c797d9d
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2026-09-24 10:28:14 -0300

    Se anadio congnitojwtauthenticator en el producto-service

--- producto-service/src/main/java/com/peluchin/producto_service/config/SecurityConfig.java
+++ producto-service/src/main/java/com/peluchin/producto_service/config/SecurityConfig.java
@@ -8,13 +8,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final CognitoJwtAuthenticationConverter jwtAuthenticationConverter;
+
+    public SecurityConfig(
+            CognitoJwtAuthenticationConverter jwtAuthenticationConverter) {
+        this.jwtAuthenticationConverter = jwtAuthenticationConverter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
+
                 .authorizeHttpRequests(auth -> auth
+
                         // Catálogo público
                         .requestMatchers(
                                 HttpMethod.GET,
@@ -22,14 +31,34 @@ public class SecurityConfig {
                                 "/api/v1/productos/**"
                         ).permitAll()
 
-                        // Todo lo demás requiere autenticación
+                        // Solo administrador
+                        .requestMatchers(
+                                HttpMethod.POST,
+                                "/api/v1/productos",
+                                "/api/v1/productos/**"
+                        ).hasRole("ADMINISTRADOR")
+
+                        .requestMatchers(
+                                HttpMethod.PUT,
+                                "/api/v1/productos/**"
+                        ).hasRole("ADMINISTRADOR")
+
+                        .requestMatchers(
+                                HttpMethod.DELETE,
+                                "/api/v1/productos/**"
+                        ).hasRole("ADMINISTRADOR")
+
                         .anyRequest().authenticated()
                 )
+
                 .oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt(Customizer.withDefaults())
+                        oauth2.jwt(jwt ->
+                                jwt.jwtAuthenticationConverter(
+                                        jwtAuthenticationConverter
+                                )
+                        )
                 );
 
         return http.build();
-
     }
 }
